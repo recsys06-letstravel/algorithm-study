93ac03bb91edcb37619e52ea03985377fff0f8ac
inhyeokJeon <poiu2389@naver.com>
2022-04-21 23:57:23 +0900

    다시 풀어보기

--- LeetCode/inhyeok/15_3Sum.py
+++ LeetCode/inhyeok/15_3Sum.py
@@ -1,6 +1,5 @@
 # 세수의 합 = 0
 
-
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         # brute force # Time Limit Exceeded
@@ -21,26 +20,50 @@ class Solution:
         #                 break
 
         # two point 1088ms
+        # nums.sort()
+        # result = []
+        # for i in range(len(nums)):
+        #     if i > 0 and nums[i] == nums[i - 1]:
+        #         continue
+        #     left = i+1
+        #     right = len(nums)-1
+        #     while left < right:
+        #         sums = nums[left] + nums[right] + nums[i]
+        #         if sums > 0:
+        #             right -= 1
+        #         elif sums < 0:
+        #             left += 1
+        #         else:  # sum = 0
+        #             result.append([nums[i], nums[left], nums[right]])
+        #             # 모두 0 일떄 생각해서 없애주자
+        #             while left < right and nums[left] == nums[left+1]:
+        #                 left += 1
+        #             while left < right and nums[right] == nums[right - 1]:
+        #                 right -= 1
+        #
+        #             left += 1
+        #             right -= 1
+
+
         nums.sort()
         result = []
         for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i - 1]:
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-            left = i+1
-            right = len(nums)-1
+            left = i + 1
+            right = len(nums) - 1
             while left < right:
-                sums = nums[left] + nums[right] + nums[i]
+                sums = nums[i] + nums[left] + nums[right]
                 if sums > 0:
                     right -= 1
                 elif sums < 0:
                     left += 1
-                else:  # sum = 0
+                else:
                     result.append([nums[i], nums[left], nums[right]])
-                    # 모두 0 일떄 생각해서 없애주자
                     while left < right and nums[left] == nums[left+1]:
                         left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -=1
+                    while left < right and nums[right] == nums[right-1]:
+                        right -= 1
 
                     left += 1
                     right -= 1
--- LeetCode/inhyeok/42_trapping_rain_water.py
+++ LeetCode/inhyeok/42_trapping_rain_water.py
@@ -31,7 +31,6 @@ class Solution:
 
             stack.append(i)
 
-
         return volume
 
 
