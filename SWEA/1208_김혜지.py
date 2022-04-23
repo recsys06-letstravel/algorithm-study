31e709d47ef6a42453ab1699cf2c07a82746418c
h-y-e-j-i <11th_hj@naver.com>
2022-04-23 23:53:01 +0900

    숏코딩

--- SWEA/1208_김혜지.py
+++ SWEA/1208_김혜지.py
@@ -1,20 +1,9 @@
 # https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=3&contestProbId=AV139KOaABgCFAYh&categoryId=AV139KOaABgCFAYh&categoryType=CODE&problemTitle=&orderBy=SUBMIT_COUNT&selectCodeLang=PYTHON&select-1=3&pageSize=10&pageIndex=1&&&&&&&&&&
 # Flatten
 
-T = 10
-
-for test_case in range(1, T + 1):
-    dump_count = int(input())
-    yellow_boxes = list(map(int, input().strip().split()))
-
+for test_case in range(1, 11):
+    dump_count, yellow_boxes = int(input()), list(map(int, input().strip().split()))
     for _ in range(dump_count):
-        max_height = max(yellow_boxes)
-        max_idx = yellow_boxes.index(max_height)
-
-        min_height = min(yellow_boxes)
-        min_idx = yellow_boxes.index(min_height)
-
-        yellow_boxes[max_idx] -= 1
-        yellow_boxes[min_idx] += 1
-
+        yellow_boxes[yellow_boxes.index(max(yellow_boxes))] -= 1
+        yellow_boxes[yellow_boxes.index(min(yellow_boxes))] += 1
     print(f"#{test_case} {max(yellow_boxes)-min(yellow_boxes)}")
